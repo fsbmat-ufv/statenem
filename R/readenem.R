22d76c21f670d6a48e19f2ff877ea8f28b96d10d
fsbmat-ufv <fernando.bastos@ufv.br>
2021-05-14 23:18:22 -0300

    read enem

--- R/readenem.R
+++ R/readenem.R
@@ -1,4 +1,4 @@
-#' @title Faz o download do banco de dados do Enem
+#' @title Faz a leitura do banco de dados do Enem
 #' @name readenem
 #'
 #' @description Funcao para fazer a leitura dos
@@ -29,11 +29,18 @@ readenem <- function(ano){
   } else {
     df <- "MICRODADOS_ENEM_2019.csv"
   }
-  dados <- data.table::fread(input=df,
+  dados <- data.table::fread(input='df',
                              integer64='character',
                              skip=0, #Ler do inicio
-                             nrow=1000, #Quantidade de registros a serem lidos
+                             nrow=-1, #Ler todos os registros
                              na.strings = "",
                              showProgress = TRUE, encoding = "Latin-1",
-                             header = TRUE)
+                             header = TRUE, select = c(1,2,4,6:11,16:18,20,83:86,91:94,99,104:137)) %>%
+    filter(TP_PRESENCA_CN=="1",TP_PRESENCA_CH=="1",TP_PRESENCA_LC=="1",TP_PRESENCA_MT=="1")
+  dados$nt_final <- (rowSums(dados[,c("NU_NOTA_CN", "NU_NOTA_CH", "NU_NOTA_LC", "NU_NOTA_MT", "NU_NOTA_REDACAO")]))/5
+  dados <- dados[!is.na(dados$nt_final),]
+  summary(dados$nt_final)
+  dados$nt_cat <- cut(dados$nt_final,
+                      breaks=c(-Inf, 317.58, 452.735, 587.89, 723.045, 858.2),
+                      labels=c("0" , "1", "2", "3", "4"))
 }
