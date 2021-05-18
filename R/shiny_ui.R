e6a386bb2e87412d21167668d4fc9fdeaede4cc2
fsbmat-ufv <fernando.bastos@ufv.br>
2021-05-18 12:51:14 -0300

    clear

--- R/shiny_ui.R
+++
@@ -1,9 +0,0 @@
-#' The Shiny App UI.
-#' @export
-shiny_ui <- function() {
-  fluidPage(
-    titlePanel('Shiny Parameter Test'),
-    verbatimTextOutput('environment'),
-    tableOutput('thedata')
-  )
-}
